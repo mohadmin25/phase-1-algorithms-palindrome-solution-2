b2a167d5f032160039065765ff3397665f2199a3
mohadmin25 <abuhamar008@gmail.com>
2022-08-27 18:15:44 +0300

    completed

--- index.js
+++ index.js
@@ -1,14 +1,35 @@
+
+
 function isPalindrome(word) {
-  // Write your algorithm here
+  for (let i = 0; i< word.length /2; i++){
+    const j = word.length - 1 -i  // when i= 0 j = -1 and thats y (1- i)
+    const firstLetter = word[i]
+    const lastLatter = word[j]
+    if (firstLetter !== lastLatter)
+    return false
+  }
+  return true
 }
+console.log(isPalindrome('aaaa'))
 
 /* 
-  Add your pseudocode here
+  by checking on letter
+   r a c e c a r  = letters of racecar
+   1 2 3 4 5 6 7  = numbers representing the letters of racecar
+   i              = represents the numbers moving forward(+)
+               j  = represents the numbers moving backward (-)
+  r a c e c a r
+  1 2 3 4 5 6 7
+    i       j
+  r a c e c a r
+  1 2 3 4 5 6 7
+      i   j
+  r a c e c a r
+  1 2 3 4 5 6 7
+        ij
 */
 
-/*
-  Add written explanation of your solution here
-*/
+
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
